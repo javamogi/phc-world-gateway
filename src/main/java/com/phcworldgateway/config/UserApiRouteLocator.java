8c512ad4e656e02da36a67aa1f2e12937cd9ae5a
javamogi@gmail.com <40781237+javamogi@users.noreply.github.com>
2024-01-04 20:51:00 +0900

    feat: spring cloud bus 추가

--- src/main/java/com/phcworldgateway/config/UserApiRouteLocator.java
+++ src/main/java/com/phcworldgateway/config/UserApiRouteLocator.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -18,10 +19,34 @@ public class UserApiRouteLocator {
     public RouteLocator userRouteLocator(RouteLocatorBuilder builder){
         return builder.routes()
                 .route("user-api-service",
-                        r -> r.path(gatewayPath + "**")
-                        .filters(f ->
-                                    f.rewritePath(gatewayPath + "(?<servicePath>.*)", "/${servicePath}"))
-                        .uri(userBaseUrl))
+                        r -> r.method(HttpMethod.POST)
+                                .and()
+                                .path(gatewayPath + "users/login")
+                                .filters(f ->
+                                    f.rewritePath(gatewayPath + "(?<servicePath>.*)", "/${servicePath}")
+                                            .removeResponseHeader("Cookie"))
+                                .uri(userBaseUrl))
+                .route("user-api-service",
+                        r -> r.method(HttpMethod.POST)
+                                .and()
+                                .path(gatewayPath + "users")
+                                .filters(f ->
+                                        f.rewritePath(gatewayPath + "(?<servicePath>.*)", "/${servicePath}")
+                                                .removeResponseHeader("Cookie"))
+                                .uri(userBaseUrl))
+                .route("user-api-service",
+                        r -> r.path(gatewayPath + "users/**")
+                                .filters(f ->
+                                        f.rewritePath(gatewayPath + "(?<servicePath>.*)", "/${servicePath}"))
+                                .uri(userBaseUrl))
+                .route("user-api-service",
+                        r -> r.path(gatewayPath + "actuator/**")
+                                .and()
+                                .method(HttpMethod.GET, HttpMethod.POST)
+                                .filters(f -> f.rewritePath(gatewayPath + "(?<servicePath>.*)", "/${servicePath}")
+                                        .removeResponseHeader("Cookie"))
+                                .uri(userBaseUrl))
                 .build();
     }
+
 }
